0969220588922a13ec113ef1749364303d02b602
Yifei Hu <huyifei23@gmail.com>
2019-06-20 14:20:35 +0800

    init script for final processing

---
+++ parse.py
@@ -0,0 +1,51 @@
+# -*- coding: UTF-8 -*-
+
+import json
+import os
+import pandas as pd
+import sqlite3
+
+def to_sql():
+    """
+    Write the json files into a sql db.
+    """
+    conn = sqlite3.connect('kunshan')
+    c = conn.cursor()
+    jsons = os.listdir('data/')
+    jsons = list(filter(lambda f: '.json'==f[-5:], jsons))
+    num_files = len(jsons)
+    print('Detected {} json files under data/, reading...'.format(num_files))
+    #print(jsons)
+    results = []
+    for j in jsons:
+        with open('data/'+j, 'r') as f:
+            result = json.load(f)
+            f.close()
+        #print(result)
+        results.append((result['url'], result['title'], result['date'], result['author'], result['keywords']))
+    
+    print('Executing bulk inserts...')
+    c.executemany('INSERT INTO article VALUES (?, ?, ?, ?, ?)', results)
+    conn.commit()
+    conn.close()
+    print('SQL write complete')
+
+def to_excel():
+    """
+    Read from sql db and write the results to a excel file.
+    """
+    conn = sqlite3.connect('kunshan')
+    query = 'SELECT url, title, date, author, keywords FROM main.article'
+    print('Reading from DB...')
+    kunshan = pd.read_sql(query, conn, parse_dates=['date'])
+    print('done')
+    # TODO sort by date
+    #print(kunshan)
+    conn.close()
+    print('Writing to Excel...')
+    kunshan.to_excel(u'汇总.xlsx', na_rep='NA', index=False)
+    print('done')
+
+if __name__ == "__main__":
+    #to_sql()
+    to_excel()
